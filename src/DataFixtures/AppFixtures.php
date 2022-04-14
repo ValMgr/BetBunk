2ef3ed8b9c831995b088031fb02b0759ba636887
Valentin Magry <valou32720@gmail.com>
2022-04-14 15:57:37 +0200

    Add 3 users to fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -12,6 +12,27 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@betbunk.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword('password');
+        $manager->persist($admin);
+
+        $dev = new User();
+        $dev->setUsername('dev');
+        $dev->setEmail('dev@betbunk.com');
+        $dev->setRoles(['ROLE_ADMIN']);
+        $dev->setPassword('password');
+        $manager->persist($dev);
+
+        $user1 = new User();
+        $user1->setUsername('user1');
+        $user1->setEmail('user1@betbunk.com');
+        $user1->setRoles(['ROLE_USER']);
+        $user1->setPassword('password');
+        $manager->persist($user1);
+
         $manager->flush();
     }
 }
