fe6cd7226e362bf44addff4fb23ac766f7fd6f28
Valentin Magry <valou32720@gmail.com>
2022-04-28 16:42:11 +0200

    Remove comments & refacto

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -25,23 +25,9 @@ class AdminController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
 
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
-
-        return parent::index();
     }
 
     public function configureDashboard(): Dashboard
@@ -52,8 +38,6 @@ class AdminController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
         return [
             MenuItem::linkToDashboard('Dashboard', 'fa-solid fa-screwdriver-wrench'),
             MenuItem::linktoRoute('Back to the website', 'fa fa-home', 'homepage'),
